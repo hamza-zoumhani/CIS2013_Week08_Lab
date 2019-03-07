0f75214d8ffc3b774dd46c7c8ab70fbc202fe459
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-03-06 19:29:14 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,43 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-void cat(){
-	cout << "Roar!!" << endl;
-}
 
+class Cat{
+	public:
+		
+		int size=0;
+		string breed= "house";
+		
+		void roar(){
+			cout << "The " << size << " inch " << breed
+			<< " cat says ... Roar!! " << endl;
+		}
+};
+
+void newCat(Cat cougar){
+	cougar.siee = 62;
+	cougar.breed = "cougar";
+	cougar.roar();
+	
+}
 
 int main(){
 	
+	newCat();
+	
+	Cat tiger;
+	Cat house;
+	
 	cout << "We have a cat" << endl;
-	cat();
+	tiger.breed = "tiger";
+	tiger.size = 45;
+	tiger.roar();
+	
+	cout << "Cat 2 is created" << endl;
+	house.breed = "house";
+	house.size = 33;
+	house.roar();
 	
 	return 0;
 }
